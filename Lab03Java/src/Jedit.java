e9b2c627c6bc754da67c14666d3b979e1caf5553
fredpaquette <frederic_paquette@hotmail.com>
2015-02-09 20:50:41 -0500

    lab3 debut java

---
+++ Lab03Java/src/Jedit.java
@@ -0,0 +1,69 @@
+import java.io.Console;
+
+/**
+ * Created by Razma on 2015-02-09.
+ */
+public class Jedit
+{
+
+   private void WithNumber(String fileName)
+   {
+
+
+
+
+   }
+    private void WithoutNumber(String fileName) {
+
+        java.io.BufferedReader reader = new java.io.BufferedReader(
+                new java.io.InputStreamReader(System.in));
+        String ligne = new String();
+        try {
+            java.io.PrintWriter writer = new java.io.PrintWriter(
+                    new java.io.BufferedWriter(new java.io.FileWriter(fileName)));
+            while (ligne.length() == 0) {
+                ligne = reader.readLine();
+                writer.println(ligne);
+            }
+
+
+
+
+            writer.close();
+
+        }
+
+        catch(java.io.IOException  e)
+        {
+            System.out.println("Erreur lors de la lectureS");
+        }
+
+
+
+    }
+
+    public void editer( String args[] )
+    {
+
+        if(args.length > 0)
+        {
+            if(args[0] == "-n")
+                WithNumber(args[1]);
+            else
+                WithoutNumber(args[0]);
+        }
+
+
+
+
+
+
+    }
+
+    public static void main( String args[] )
+    {
+        Jedit app = new Jedit();
+        app.editer( args );
+    }
+
+}
